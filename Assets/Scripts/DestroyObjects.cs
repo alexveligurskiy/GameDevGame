72e458477254136507e27c76b0f1039c08d9b112
1 <a1@MacBookPro.local>
2017-06-25 22:25:28 +0300

    DestroyObjectsAndAutoGenerationAttempt

---
+++ Assets/Scripts/DestroyObjects.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObjects : MonoBehaviour {
+
+	void OnBecameInvisible(){
+		
+		Debug.Log(this.gameObject.name);
+		Destroy(this.gameObject);
+	}
+}

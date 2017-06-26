882c12aafc1b2ce243b8c879fca181a103dda9f3
1 <a1@MacBookPro.local>
2017-06-26 14:14:44 +0300

    StartSceneAndSomeGui

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour {
+
+	public MyButton PlayButton;
+
+	void Start () {
+		PlayButton.signalOnClick.AddListener (this.onPlay);
+	}
+	void onPlay() {
+		//Do something
+		SceneManager.LoadScene("GameScene");
+	}
+}
